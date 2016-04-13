aad0c63e0606934b7287a303c29bcebc166b841c
brunabrk <brunabrk@gmail.com>
2016-04-13 18:47:12 -0300

    Refactoring the hook example rb to have before

--- part_1/spec/lib/cat_example_hook_spec.rb
+++ part_1/spec/lib/cat_example_hook_spec.rb
@@ -3,14 +3,14 @@ require "cat"
 
 describe Cat do
   let(:cat) { Cat.new }
-  
+  #default: before(:each)
+  before { cat.hungry? }
+
 	it 'it is hungry' do
-		cat.hungry?
 		cat.should be_hungry
 	end
 
 	it 'eats mouses' do
-		cat.hungry?
 		cat.should be_eating_mouses
 	end
 end
